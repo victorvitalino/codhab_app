e3896f5274c03d3b4ec7c0aef680bc9ea6e79d87
Victor Vitalino <vitalino.victor@gmail.com>
2016-10-19 14:20:00 -0200

    fix

--- www/js/controllers/noticias.js
+++ www/js/controllers/noticias.js
@@ -3,11 +3,13 @@ var app = angular.module('codhab.controllers.noticias', []);
 /*********************************************************************
  * NoticiasCtrl
  *********************************************************************/
-app.controller('NoticiasCtrl', function ($scope,$stateParams,$http, $sce, $parse) {
+app.controller('NoticiasCtrl', function ($scope,$stateParams,$http, $sce, $parse,$ionicSlideBoxDelegate, $state) {
+  $ionicSlideBoxDelegate.update()
   $http.get('http://www.codhab.df.gov.br/noticias.json')
         .success(function(data, status, headers,config){
           console.log(data);
-          $scope.results = data;
+          $scope.results = data[0].posts;
+          $scope.results_sliders = data[0].sliders;
 
         })
         .error(function(data, status, headers,config){
