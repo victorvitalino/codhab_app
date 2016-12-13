c611dad43f3a4e8f74550c3d301dfd6ea03947e9
Victor Vitalino <vitalino.victor@gmail.com>
2016-12-13 17:08:46 -0200

    button do capeta

--- www/js/controllers/search.js
+++ www/js/controllers/search.js
@@ -2,10 +2,49 @@ var app = angular.module('codhab.controllers.search', []);
 app.controller('SearchCtrl', function ($scope, $state, $ionicLoading, $http) {
   $scope.bookmark = function(search){
   localStorage.setItem("bookmark", search.cpf);
-
-  console.log(localStorage['bookmark']);
   };
 
+  $scope.searchBookmark = function(){
+    var bookmark = localStorage['bookmark'];
+    $scope.verify = "";
+    $scope.result = "";
+    $scope.results = "";
+    $scope.result_indication = "";
+    $ionicLoading.show({
+      content: 'Loading',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
+    });
+    if (search.cpf === undefined){
+      console.log('carai')
+    }else{
+      console.log(search.cpf)
+    }
+    $http.get('http://extranet.codhab.df.gov.br/candidato/cadastros/'+ bookmark +'.json?token=eed6a8780692be1675b1bd0f386ca8b0')
+      .success(function (data, status, headers, config) {
+        var old = moment().diff(moment(data.born), 'years');
+        if (old >= 60) {
+          data.olds = "Idoso";
+        } else {
+          data.olds = "Não";
+        }
+        console.log(data);
+        data.bookmark = localStorage['bookmark'];
+        $scope.result = data;
+        $ionicLoading.hide();
+      })
+      .error(function (data, status, headers, config) {
+        console.log('data error');
+         $scope.verify = true;
+         $ionicLoading.hide();
+      })
+      .then(function (result) {
+        things = result.data;
+        $ionicLoading.hide();
+      });
+  };
   $scope.searchCPF = function (search) {
 
     $scope.verify = "";
