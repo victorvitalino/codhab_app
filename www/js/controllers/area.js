6c2e355bf3fb87bd88f0ebce933f5a642d02ef9e
Victor Vitalino <vitalino.victor@gmail.com>
2017-02-21 14:48:02 -0300

    fix e logout fix

--- www/js/controllers/area.js
+++ www/js/controllers/area.js
@@ -1,15 +1,25 @@
 var app = angular.module('codhab.controllers.area', []);
-app.controller('AreaCtrl', function($scope,$ionicPlatform, $ionicConfig, $state,$sce,$filter) {
+app.controller('AreaCtrl', function($scope,$ionicPlatform, $ionicConfig, $state,$sce,$filter,$http) {
 
 
    $scope.cpf_logados = localStorage['cpf_logado'];
-   console.log($scope.cpf_logados);
+  //  console.log($scope.cpf_logados);
    $scope.url = $sce.trustAsResourceUrl("http://www.codhab.df.gov.br/candidato/area_restrita?cpf="+$scope.cpf_logados);
    $scope.logout = function(){
+     //  console.log($scope.cpf_logados);
+
+     $http.post("http://www.codhab.df.gov.br/autentica?cpf="+$scope.cpf_logados+'=false')
+     .success(function (data, status, headers, config){
+       console.log(data)
+     }).error(function(data, status, headers, config){
+     }).then(function(result){
+     });
      window.localStorage['cpf_logado'] = '';
      $scope.cpf_logados = '';
+
      $state.go('tabs.home');
      window.location.reload();
+
    }
 
 });
