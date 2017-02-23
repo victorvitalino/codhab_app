6c99fb9058d9763e0ad934252a31ef55fd339cc3
Victor Vitalino <vitalino.victor@gmail.com>
2017-02-23 10:31:14 -0300

    fix

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('codhab.controllers.app', []);
-app.controller('AppCtrl', function($scope,$ionicPlatform, $ionicConfig, $state, $cordovaGeolocation,$cordovaBadge,$interval) {
+app.controller('AppCtrl', function($scope, $ionicConfig, $state, $cordovaGeolocation, $cordovaLocalNotification, $ionicPlatform) {
 
    /** Inicio da antiga geolocalização **/
 
@@ -15,7 +15,63 @@ app.controller('AppCtrl', function($scope,$ionicPlatform, $ionicConfig, $state, 
      alert("Aviso: O aplicativo CODHAB utiliza o GPS para localizar Postos e Entidades. Por favor ative seu GPS.");
    });
 
+   $ionicPlatform.ready(function () {
 
+   		$scope.scheduleSingleNotification = function () {
+   		  $cordovaLocalNotification.schedule({
+   			id: 1,
+   			title: 'Warning',
+   			text: 'Youre so sexy!',
+   			data: {
+   			  customProperty: 'custom value'
+   			}
+   		  }).then(function (result) {
+   			console.log('Notification 1 triggered');
+   		  });
+   		};
+
+   		$scope.scheduleDelayedNotification = function () {
+   		  var now = new Date().getTime();
+   		  var _10SecondsFromNow = new Date(now + 10 * 1000);
+
+   		  $cordovaLocalNotification.schedule({
+   			id: 2,
+   			title: 'Warning',
+   			text: 'Im so late',
+   			at: _10SecondsFromNow
+   		  }).then(function (result) {
+   			console.log('Notification 2 triggered');
+   		  });
+   		};
+
+   		$scope.scheduleEveryMinuteNotification = function () {
+   		  $cordovaLocalNotification.schedule({
+   			id: 3,
+   			title: 'Warning',
+   			text: 'Dont fall asleep',
+   			every: 'minute'
+   		  }).then(function (result) {
+   			console.log('Notification 3 triggered');
+   		  });
+   		};
+
+   		$scope.updateSingleNotification = function () {
+   		  $cordovaLocalNotification.update({
+   			id: 2,
+   			title: 'Warning Update',
+   			text: 'This is updated text!'
+   		  }).then(function (result) {
+   			console.log('Notification 1 Updated');
+   		  });
+   		};
+
+   		$scope.cancelSingleNotification = function () {
+   		  $cordovaLocalNotification.cancel(3).then(function (result) {
+   			console.log('Notification 3 Canceled');
+   		  });
+   		};
+
+   	});
 
   //
   //  var
