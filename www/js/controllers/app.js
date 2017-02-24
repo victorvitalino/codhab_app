eb860862fa26541bb05acacba7f7f0304583ad7b
Victor Vitalino <vitalino.victor@gmail.com>
2017-02-24 16:33:43 -0300

    gps

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -1,8 +1,8 @@
 var app = angular.module('codhab.controllers.app', []);
-app.controller('AppCtrl', function($scope, $ionicConfig, $state, $cordovaGeolocation, $cordovaLocalNotification, $ionicPlatform) {
+app.controller('AppCtrl', function($scope, $ionicConfig, $state, $http, $cordovaGeolocation, $ionicPlatform) {
 
    /** Inicio da antiga geolocalização **/
-
+    $scope.cpf_envio = localStorage['cpf_logado'];
     $cordovaGeolocation.getCurrentPosition({timeout:30000, maximumAge:3000, enableHighAccuracy:false})
     .then(function(position){
      $scope.lat = position.coords.latitude;
@@ -10,238 +10,62 @@ app.controller('AppCtrl', function($scope, $ionicConfig, $state, $cordovaGeoloca
      window.localStorage['lat'] = $scope.lat;
      window.localStorage['long'] = $scope.long;
      console.log($scope.lat);
-     // http://www.codhab.df.gov.br/lat-lng?lat=123&lng=123&cpf=123
    }, function (err){
      alert("Aviso: O aplicativo CODHAB utiliza o GPS para localizar Postos e Entidades. Por favor ative seu GPS.");
    });
 
    $ionicPlatform.ready(function () {
+     document.addEventListener('deviceready', onDeviceReady, false);
 
-   		$scope.scheduleSingleNotification = function () {
-   		  $cordovaLocalNotification.schedule({
-   			id: 1,
-   			title: 'Warning',
-   			text: 'Youre so sexy!',
-   			data: {
-   			  customProperty: 'custom value'
-   			}
-   		  }).then(function (result) {
-   			console.log('Notification 1 triggered');
-   		  });
-   		};
+     function onDeviceReady () {
 
-   		$scope.scheduleDelayedNotification = function () {
-   		  var now = new Date().getTime();
-   		  var _10SecondsFromNow = new Date(now + 10 * 1000);
+           /**
+           * This callback will be executed every time a geolocation is recorded in the background.
+           */
+           var callbackFn = function(location) {
+               console.log('[js] BackgroundGeolocation callback:  ' + location.latitude + ',' + location.longitude);
+               // Do your HTTP request here to POST location to your server.
+               // jQuery.post(url, JSON.stringify(location));
 
-   		  $cordovaLocalNotification.schedule({
-   			id: 2,
-   			title: 'Warning',
-   			text: 'Im so late',
-   			at: _10SecondsFromNow
-   		  }).then(function (result) {
-   			console.log('Notification 2 triggered');
-   		  });
-   		};
+               /*
+               IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+               and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+               IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+               */
 
-   		$scope.scheduleEveryMinuteNotification = function () {
-   		  $cordovaLocalNotification.schedule({
-   			id: 3,
-   			title: 'Warning',
-   			text: 'Dont fall asleep',
-   			every: 'minute'
-   		  }).then(function (result) {
-   			console.log('Notification 3 triggered');
-   		  });
-   		};
+               backgroundGeolocation.finish();
+           };
 
-   		$scope.updateSingleNotification = function () {
-   		  $cordovaLocalNotification.update({
-   			id: 2,
-   			title: 'Warning Update',
-   			text: 'This is updated text!'
-   		  }).then(function (result) {
-   			console.log('Notification 1 Updated');
-   		  });
-   		};
+           var failureFn = function(error) {
+               console.log('BackgroundGeolocation error');
+           };
 
-   		$scope.cancelSingleNotification = function () {
-   		  $cordovaLocalNotification.cancel(3).then(function (result) {
-   			console.log('Notification 3 Canceled');
-   		  });
-   		};
+           // BackgroundGeolocation is highly configurable. See platform specific configuration options
+           backgroundGeolocation.configure(callbackFn, failureFn, {
+               desiredAccuracy: 10,
+               stationaryRadius: 5,
+               distanceFilter:5,
+               interval: 60000,
+               debug:true,
+               notificationTitle: 'CODHAB',
+               notificationText: 'ativo',
+               stopOnTerminate: false,
+               startOnBoot: true,
+               stopOnStillActivity: false,
+               notificationIconLarge:'icon',
+              //  notificationIconSmall:'icon',
+               url: 'http://www.codhab.df.gov.br/lat-lng?cpf='+ $scope.cpf_envio,
+               httpHeaders: {
+                 'cpf': $scope.cpf_envio
+              }
+           });
+
+           // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+           backgroundGeolocation.start();
+
+           // If you wish to turn OFF background-tracking, call the #stop method.
+           // backgroundGeolocation.stop();
+       }
 
    	});
-
-  //
-  //  var
-  //      map,
-  //      previousLocation,
-  //      locationMarkers = [],
-  //      stationaryCircles = [],
-  //      currentLocationMarker,
-  //      locationAccuracyCircle,
-  //      path,
-  //      userStartIntent,
-  //      isStarted = true,
-  //      isLocationEnabled = true,
-  //      configHasChanges = true;
-  //
-  //
-  //  /** Inicio da nova geolocalização **/
-  //  document.addEventListener("deviceready", onDeviceReady, false);
-  //   function onDeviceReady() {
-  //     //  console.log('foi')
-  //     $cordovaBackgroundGeolocation.start();
-  //     var callbackFn = function(location) {
-  //           $http({
-  //               //request options to send data to server
-  //           });
-  //         $cordovaBackgroundGeolocation.finish();
-  //       },
-  //       failureFn = function(error) {
-  //         console.log('BackgroundGeoLocation error ' + JSON.stringify(error));
-  //       }
-  //
-  //     backgroundGeolocation.configure(callbackFn, failureFn, {
-  //       desiredAccuracy: 10,
-  //       stationaryRadius: 20,
-  //       distanceFilter: 30,
-  //       url: 'http://192.168.81.15:3000/locations',
-  //       httpHeaders: { 'X-FOO': 'bar' },
-  //       maxLocations: 1000,
-  //       // Android only section
-  //       locationProvider: 0,
-  //       interval: 60000,
-  //       fastestInterval: 5000,
-  //       activitiesInterval: 10000,
-  //       notificationTitle: 'Background tracking',
-  //       notificationText: 'enabled',
-  //       notificationIconColor: '#FEDD1E',
-  //       notificationIconLarge: 'mappointer_large',
-  //       notificationIconSmall: 'mappointer_small',
-  //     });
-  //
-  //     $cordovaBackgroundGeolocation.watchLocationMode(
-  //       function (enabled) {
-  //         if (enabled) {
-  //           // location service are now enabled
-  //           // call backgroundGeolocation.start
-  //           // only if user already has expressed intent to start service
-  //           alert('start')
-  //         } else {
-  //           // location service are now disabled or we don't have permission
-  //           // time to change UI to reflect that
-  //         }
-  //       },
-  //       function (error) {
-  //         console.log('Error watching location mode. Error:' + error);
-  //       }
-  //     );
-  //
-  //     $cordovaBackgroundGeolocation.isLocationEnabled(function (enabled) {
-  //       if (enabled) {
-  //         $cordovaBackgroundGeolocation.start(
-  //           function () {
-  //             // service started successfully
-  //             // you should adjust your app UI for example change switch element to indicate
-  //             // that service is running
-  //           },
-  //           function (error) {
-  //             // Tracking has not started because of error
-  //             // you should adjust your app UI for example change switch element to indicate
-  //             // that service is not running
-  //             if (error.code === 2) {
-  //               if (window.confirm('Not authorized for location updates. Would you like to open app settings?')) {
-  //                 $cordovaBackgroundGeolocation.showAppSettings();
-  //               }
-  //             } else {
-  //               window.alert('Start failed: ' + error.message);
-  //             }
-  //           }
-  //         );
-  //       } else {
-  //         // Location services are disabled
-  //         if (window.confirm('Location is disabled. Would you like to open location settings?')) {
-  //           $cordovaBackgroundGeolocation.showLocationSettings();
-  //         }
-  //       }
-  //     });
-  // }
 });
-
-// document.addEventListener('deviceready', onDeviceReady, true);
-// console.log('inicio')
-//  function onDeviceReady () {
-//    backgroundGeolocation.configure(callbackFn, failureFn, {
-//          desiredAccuracy: 10,
-//          stationaryRadius: 20,
-//          distanceFilter: 30,
-//          url: 'http://192.168.81.15:3000/locations',
-//          httpHeaders: { 'X-FOO': 'bar' },
-//          maxLocations: 1000,
-//          startOnBoot: true,
-//          startForeground: true,
-//          stopOnTerminate: false,
-//          // Android only section
-//          locationProvider: backgroundGeolocation.provider.ANDROID_ACTIVITY_PROVIDER,
-//          interval: 6000,
-//          fastestInterval: 5000,
-//          activitiesInterval: 10000,
-//          notificationTitle: 'Background tracking',
-//          notificationText: 'enabled',
-//          notificationIconColor: '#FEDD1E',
-//          notificationIconLarge: 'mappointer_large',
-//          notificationIconSmall: 'mappointer_small'
-//      });
-//
-//      backgroundGeolocation.watchLocationMode(
-//        function (enabled) {
-//          if (enabled) {
-//            console.log('foi')
-//            // location service are now enabled
-//            // call backgroundGeolocation.start
-//            // only if user already has expressed intent to start service
-//          } else {
-//            // location service are now disabled or we don't have permission
-//            // time to change UI to reflect that
-//          }
-//        },
-//        function (error) {
-//          console.log('Error watching location mode. Error:' + error);
-//        }
-//      );
-//
-//      backgroundGeolocation.isLocationEnabled(function (enabled) {
-//        if (enabled) {
-//          backgroundGeolocation.start(
-//            function () {
-//              console.log('foi')
-//              // service started successfully
-//              // you should adjust your app UI for example change switch element to indicate
-//              // that service is running
-//            },
-//            function (error) {
-//              // Tracking has not started because of error
-//              // you should adjust your app UI for example change switch element to indicate
-//              // that service is not running
-//              if (error.code === 2) {
-//                if (window.confirm('Not authorized for location updates. Would you like to open app settings?')) {
-//                  backgroundGeolocation.showAppSettings();
-//                }
-//              } else {
-//                window.alert('Start failed: ' + error.message);
-//              }
-//            }
-//          );
-//        } else {
-//          // Location services are disabled
-//          if (window.confirm('Location is disabled. Would you like to open location settings?')) {
-//            backgroundGeolocation.showLocationSettings();
-//          }
-//        }
-//      });
-//      backgroundGeolocation.start();
-     // If you wish to turn OFF background-tracking, call the #stop method.
-     // backgroundGeolocation.stop();
- // }
