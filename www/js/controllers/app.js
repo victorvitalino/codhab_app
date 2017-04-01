da9011c249a08f2098bf3693b6670ebc0b850f83
Cristiano Cardoso <cristiano.liracardoso@gmail.com>
2017-04-01 11:19:18 -0300

    keyboard fix

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('codhab.controllers.app', []);
-app.controller('AppCtrl', function($scope, $ionicConfig, $state, $http, $cordovaGeolocation, $ionicPlatform) {
+app.controller('AppCtrl', function($scope, $ionicConfig, $state, $http, $cordovaGeolocation, $ionicPlatform, $ionicPopup) {
   $scope.cpf_toogle = localStorage['cpf_logado']
   $scope.cpf_envio = localStorage['cpf_logado'];
 
